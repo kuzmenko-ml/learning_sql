e84051a5c339843204d643ccf0900caaca44250c
fesqia <se23ex-02@stu.zsea.edu.ua>
2026-09-26 11:15:20 +0300

    feat: add new SELECT.'Q2. Get the title, album and genre of all songs by AC/DC'

--- questions.sql
+++ questions.sql
@@ -4,3 +4,10 @@ INNER JOIN album al ON al.albumid = t.albumid
 INNER JOIN artist ar ON ar.artistid = al.artistid 
 INNER JOIN genre g ON g.genreid = t.genreid
 WHERE g.name = 'Pop';
+
+SELECT t.name AS song_name, al.title AS album_title, g.name AS genre, a.name AS artist
+FROM track t
+INNER JOIN album al ON al.albumid = t.albumid
+INNER JOIN genre g ON g.genreid = t.genreid
+INNER JOIN artist a ON a.artistid = al.artistid
+WHERE a.name = 'AC/DC';
