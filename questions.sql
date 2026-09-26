b0a4328c3ddcedcc7eebc223be9e78aabefce681
fesqia <se23ex-02@stu.zsea.edu.ua>
2026-09-26 13:21:47 +0300

    feat: add new SELECT using CTE. 'Q3. Get the most purchased song of 2012 in each country'

--- questions.sql
+++ questions.sql
@@ -11,3 +11,25 @@ INNER JOIN album al ON al.albumid = t.albumid
 INNER JOIN genre g ON g.genreid = t.genreid
 INNER JOIN artist a ON a.artistid = al.artistid
 WHERE a.name = 'AC/DC';
+
+-- Тут CTE тобто віконна функція яка допомагає розвантажити складну задачу.
+-- Результат запиту це таблиця, вона не існує в БД. Але до цієї таблиці-результату можна писати інший запит.
+WITH country_track_counts AS (
+	SELECT 
+			i.billingcountry AS country,
+			t.name AS track_name, COUNT(il.trackid) AS purchase_count
+	FROM invoice i
+	INNER JOIN invoiceline il ON il.invoiceid = i.invoiceid
+	INNER JOIN track t ON t.trackid = il.trackid
+	WHERE i.invoicedate BETWEEN '2012-01-01' AND '2012-12-31'
+	GROUP BY i.billingcountry, t.name
+)
+-- Ось це якраз і є запит із підзапитом до тимчасової таблиці.
+SELECT country, track_name, purchase_count
+FROM country_track_counts
+WHERE purchase_count = (
+    SELECT MAX(sub.purchase_count)
+    FROM country_track_counts sub
+    WHERE sub.country = country_track_counts.country
+)
+ORDER BY purchase_count DESC;
